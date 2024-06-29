3302285a08c4ba46108eea192d6718e12b27a255
Haim Goldstein <haim.goldest@gmail.com>
2024-06-29 16:38:10 +0300

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyDwDODdUBUlTZWQH1n07ZgPpm9MbZvnxR4',
+    authDomain: 'ng-temp-ebffd.firebaseapp.com',
+    databaseURL:
+      'https://ng-temp-ebffd-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'ng-temp-ebffd',
+    storageBucket: 'ng-temp-ebffd.appspot.com',
+    messagingSenderId: '755412900531',
+    appId: '1:755412900531:web:9c8b8758abb4dcda8eeec6',
+    measurementId: 'G-2X5Q258P09',
+  },
+};
